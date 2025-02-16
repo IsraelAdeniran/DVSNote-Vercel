d2a7a5f0951216b983865b97cdcf31525531f58a
IsraelAdeniran <B00157067@mytudublin.ie>
2025-02-16 22:23:19 +0000

    Changes

--- my-expo-app/App.js
+++ my-expo-app/App.js
@@ -5,7 +5,7 @@ import { WebView } from 'react-native-webview';
 export default function App() {
   return (
       <WebView
-          source={{ uri: 'https://myandroidapp.vercel.app/' }}  // Link to your live Next.js app
+          source={{ uri: 'https://dvs-note-vercel.vercel.app/register' }} 
       />
   );
 }
