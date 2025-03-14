e5c12795bff1a3f03495405d196b9f266c16a91b
IsraelAdeniran <B00157067@mytudublin.ie>
2025-03-14 02:12:35 +0000

    Changes

--- dvsnote-vercel/next.config.mjs
+++ dvsnote-vercel/next.config.mjs
@@ -1,4 +1,21 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    experimental: {
+        optimizeCss: true,
+    },
+    webpack(config) {
+        config.module.rules.push({
+            test: /\.css$/,
+            exclude: /\.module\.css$/,
+            use: [
+                {
+                    loader: require.resolve("ignore-loader"),
+                },
+            ],
+        });
+
+        return config;
+    },
+};
 
 export default nextConfig;
